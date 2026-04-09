e4cf05fc3cf5aefa0e85038644cf0e834f57e3b3
Rahul Kushwaha <raahullkushwaha@gmail.com>
2026-04-09 16:48:41 +0530

    Add entities in ShowSeat, Theater and User

--- src/main/java/com/rahul/bookmyshow/model/Theater.java
+++ src/main/java/com/rahul/bookmyshow/model/Theater.java
@@ -1,4 +1,34 @@
 package com.rahul.bookmyshow.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name="theater")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Theater {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String city;
+
+    @OneToMany(mappedBy = "theater", cascade = CascadeType.ALL)
+    private List<Screen> totalScreen;
+
+    private Integer totalSeats;
 }

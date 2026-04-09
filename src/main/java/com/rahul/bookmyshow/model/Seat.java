38ad5b455e3dd700385808dcef19c5494440d785
Rahul Kushwaha <raahullkushwaha@gmail.com>
2026-04-09 16:36:11 +0530

    Add functionality in seat and show

--- src/main/java/com/rahul/bookmyshow/model/Seat.java
+++ src/main/java/com/rahul/bookmyshow/model/Seat.java
@@ -1,4 +1,30 @@
 package com.rahul.bookmyshow.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="seats")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String seatNumber;
+
+    @Column(nullable = false)
+    private String seatType; // Gold, Silver etc
+
+    @Column(nullable = false)
+    private Double basePrice;
+
+    @ManyToOne
+    @JoinColumn(name="screen", nullable = false)
+    private Screen screen;
 }
